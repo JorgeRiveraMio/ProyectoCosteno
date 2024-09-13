abbc0cdc813e18980207642a6e8b99b8fc662972
marco <marcofabianj@hotmail.com>
2024-09-13 14:52:02 -0500

    se agrego perfil

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { PrincipalComponent } from './Pages/principal/principal.component';
 import { LoginComponent } from './Pages/login/login.component';
 import { RegistroComponent } from './Pages/registro/registro.component';
+import { PerfilComponent } from './Pages/perfil/perfil.component';
 
 export const routes: Routes = [
     {
@@ -15,5 +16,8 @@ export const routes: Routes = [
       {
             path:"registro",
             component:RegistroComponent
+        },{
+            path:"perfil",
+            component: PerfilComponent
         }
 ];
